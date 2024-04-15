7c56e24ffa9f2d52c560aa0247ba267431cab847
bbbasya <42473270+asyaB404@users.noreply.github.com>
2024-04-16 05:00:13 +0800

    基本实现了玩家全部的属性加成(可以根据散射数量修改总体散射角度)，添加了摄像机跟随鼠标指针移动的功能，添加了武器（剑），且玩家的射击依赖于武器

--- Assets/Scripts/Player/PlayerStat.cs
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -2,19 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [Serializable]
 public class PlayerStat
 {
-    [SerializeField] private float val;
+    [SerializeField]
+    private float val;
     public List<float> changes;
+
     public float GetValue()
     {
         float res = val;
-        foreach (float c in changes) res += c;
+        foreach (float c in changes)
+            res += c;
         return res > 0 ? res : 0;
     }
+
     public void AddChange(float change)
     {
         changes.Add(change);
+    }
+
+    public void Clear()
+    {
+        changes.Clear();
     }
 }
