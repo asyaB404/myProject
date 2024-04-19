5500c01985e1bf54b24041ecd79aa69df18fa97b
bbbasya <42473270+asyaB404@users.noreply.github.com>
2024-04-19 16:34:18 +0800

    更新 LevelManager.cs
    完善关卡样例

--- Assets/Scripts/Other/LevelManager.cs
+++ Assets/Scripts/Other/LevelManager.cs
@@ -10,8 +10,8 @@ public class LevelManager : MonoBehaviour
     {
         get => instance;
     }
-    public int level = 1;
-    public float timer = 20;
+    public int level = 0;
+    public float timer = 0;
     public bool isStart = true;
 
     private void Awake()
@@ -45,6 +45,11 @@ public class LevelManager : MonoBehaviour
         );
     }
 
+    private void Start()
+    {
+        // StartNextLevel();
+    }
+
     private void Update()
     {
         if (isStart)
@@ -74,6 +79,57 @@ public class LevelManager : MonoBehaviour
             timer = 50;
         else if (level == 20)
             timer = 60;
+        if (level == 1)
+        {
+            SpawnEnemyCoroutine(1, 2);
+            SpawnEnemyCoroutine(2, 2);
+            SpawnEnemyCoroutine(7, 2);
+            SpawnEnemyCoroutine(8, 2);
+        }
+        else if (level == 2)
+        {
+            SpawnEnemyCoroutine(1, 1);
+            SpawnEnemyCoroutine(2, 1);
+            SpawnEnemyCoroutine(3, 3);
+            SpawnEnemyCoroutine(4, 3);
+            SpawnEnemyCoroutine(7, 1);
+            SpawnEnemyCoroutine(8, 1);
+            SpawnEnemyCoroutine(9, 3);
+            SpawnEnemyCoroutine(10, 3);
+        }
+        else if (level == 3)
+        {
+            SpawnEnemyCoroutine(1, 0.6f);
+            SpawnEnemyCoroutine(2, 0.6f);
+            SpawnEnemyCoroutine(3, 3);
+            SpawnEnemyCoroutine(4, 3);
+            SpawnEnemyCoroutine(7, 0.6f);
+            SpawnEnemyCoroutine(8, 0.6f);
+            SpawnEnemyCoroutine(9, 3);
+            SpawnEnemyCoroutine(10, 3);
+        }
+        else if (level == 4)
+        {
+            SpawnEnemyCoroutine(1, 1);
+            SpawnEnemyCoroutine(2, 1);
+            SpawnEnemyCoroutine(3, 2);
+            SpawnEnemyCoroutine(4, 2);
+            SpawnEnemyCoroutine(7, 1);
+            SpawnEnemyCoroutine(8, 1);
+            SpawnEnemyCoroutine(9, 2);
+            SpawnEnemyCoroutine(10, 2);
+        }
+        else if (level == 5)
+        {
+            SpawnEnemyCoroutine(1, 0.5f);
+            SpawnEnemyCoroutine(2, 0.5f);
+            SpawnEnemyCoroutine(3, 1);
+            SpawnEnemyCoroutine(4, 1);
+            SpawnEnemyCoroutine(7, 1);
+            SpawnEnemyCoroutine(8, 1);
+            SpawnEnemyCoroutine(9, 2);
+            SpawnEnemyCoroutine(10, 2);
+        }
     }
 
     public void LevelClear()
